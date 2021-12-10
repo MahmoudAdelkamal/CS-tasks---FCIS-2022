fee46ff67aa3078a7c7884df67a4e73065cacb8d
Mahmoud Alaa <46302024+Astaa001@users.noreply.github.com>
2021-12-10 02:21:28 +0200

    added training model time calculation

--- Machine Learning/Milestone 1/Model_1.py
+++ Machine Learning/Milestone 1/Model_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import pandas as pd
 from Pre_Processing import *
 from sklearn.model_selection import cross_val_score
@@ -9,15 +10,17 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LinearRegression
 
 Features, Sales = Pre_processing()
-X_train, X_test, y_train, y_test = train_test_split(Features, Sales, test_size=0.30, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(Features, Sales, test_size=0.30, shuffle=True)
 
 multiple_linear_regression = LinearRegression()
-
+# to calculate training time
+start=time.time()
 multiple_linear_regression.fit(X_train, y_train)
-
+stop=time.time()
 y_pred = multiple_linear_regression.predict(X_test)
 
 err = metrics.mean_squared_error(y_test, y_pred)
 Accuracy = metrics.r2_score(y_test, y_pred)
 print('Model 1 Test Mean Square Error:', err)
 print('Accuracy : ', Accuracy * 100, '%')
+print(f"Training time: {stop - start}s")
--- Machine Learning/Milestone 1/Model_2.py
+++ Machine Learning/Milestone 1/Model_2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from Pre_Processing import *
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
@@ -11,17 +12,17 @@ Features, Sales = Pre_processing()
 # Split the data to training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(Features, Sales, test_size=0.30, shuffle=False)
 
-model_1_poly_features = PolynomialFeatures(degree=2)
-# transforms the existing features to higher degree features.
-X_train_poly_model_1 = model_1_poly_features.fit_transform(X_train)
-# fit the transformed features to Linear Regression
-poly_model1 = linear_model.LinearRegression()
-scores = cross_val_score(poly_model1, X_train_poly_model_1, y_train, scoring='neg_mean_squared_error', cv=5)
+Model2_poly_features = PolynomialFeatures(degree=2)
+X_train_poly_Model_2 = Model2_poly_features.fit_transform(X_train)
+poly_Model2 = linear_model.LinearRegression()
+scores = cross_val_score(poly_Model2, X_train_poly_Model_2, y_train, scoring='neg_mean_squared_error', cv=5)
 model_1_score = abs(scores.mean())
-poly_model1.fit(X_train_poly_model_1, y_train)
-
+#to calculate tarining time
+start=time.time()
+poly_Model2.fit(X_train_poly_Model_2, y_train)
+stop=time.time()
 # predicting on test data-set
-prediction = poly_model1.predict(model_1_poly_features.fit_transform(X_test))
-Model_1_Mean_Square_Error = metrics.mean_squared_error(y_test, prediction)
+prediction = poly_Model2.predict(Model2_poly_features.fit_transform(X_test))
 print('Model 2 Test Mean Square Error', metrics.mean_squared_error(y_test, prediction))
 print('Accuracy : ', metrics.r2_score(y_test, prediction) * 100, "%")
+print(f"Training time: {stop - start}s")
