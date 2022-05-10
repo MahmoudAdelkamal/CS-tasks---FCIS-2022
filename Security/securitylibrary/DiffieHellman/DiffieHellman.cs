6f063c13cacf3b62db31756b8d7e5acf9897fd7c
malaa4085 <mahmoudadel2310@gmail.com>
2022-05-10 13:49:17 +0200

    Added Hellman algorithm

--- Security/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ Security/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -8,9 +8,26 @@ namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman 
     {
+        public long FastPower(long Base, long exponential, long mod)
+        {
+            if (exponential == 0)
+                return 1;
+            long ret = FastPower(Base, exponential / 2, mod);
+            ret = (ret * ret) % mod;
+            if (exponential % 2 != 0)
+                ret = (ret * (Base % mod)) % mod;
+            return ret;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            long ya = FastPower(alpha, xa, q);
+            long yb = FastPower(alpha, xb, q);
+            List<int> Keys = new List<int>{
+
+                (int)FastPower(ya, xb, q),
+                (int)FastPower(yb, xa, q)
+            };
+            return Keys;
         }
     }
 }
